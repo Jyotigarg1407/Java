230f22d79aea21b21ddf15eb9ddb68c5e4e9b79b
Jyotigarg1407 <111676799+Jyotigarg1407@users.noreply.github.com>
2023-10-01 09:52:57 +0530

    Create binary_search.java

---
+++ binary_search.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the sorted array elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the target value to search: ");
+        int target = scanner.nextInt();
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+
+        scanner.close();
+    }
+
+    // Binary Search method
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Element found, return its index
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search the right half
+            } else {
+                right = mid - 1; // Search the left half
+            }
+        }
+
+        return -1; // Element not found
+    }
+}
